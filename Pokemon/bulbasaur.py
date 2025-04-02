c9d354406898011c4c74adcc9e61c95fba37b0cb
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2025-04-02 11:28:07 -0600

    Sobreescribe método para impresión de tipo

--- Pokemon/bulbasaur.py
+++ Pokemon/bulbasaur.py
@@ -6,3 +6,7 @@ class Bulbasaur(TipoHierba, TipoVenenoso):
     def __init__(self, defensa=111, ataque=118):
         TipoHierba.__init__(self, "Bulbasaur", defensa, ataque)
         TipoVenenoso.__init__(self, "Bulbasaur", defensa, ataque)
+        self.tipo = "Hierba / Venenoso"
+
+    def __str__(self):
+        return f"{self.nombre} ({self.tipo}) 🌱 / ☠️"
