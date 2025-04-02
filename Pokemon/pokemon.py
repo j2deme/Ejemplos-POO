283c4d282105c4f1968e6a937a8143e2f39ebd08
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2025-04-02 11:27:31 -0600

    Agrega icono para el tipo de Pokemon
    Agrega una mejora visual para demostrar el uso de diccionarios en Python, así como para reflejar la herencia hacia subclases

--- Pokemon/pokemon.py
+++ Pokemon/pokemon.py
@@ -6,4 +6,24 @@ class Pokemon:
         self.tipo = tipo
 
     def __str__(self):
-        return f"{self.nombre} ({self.tipo})"
+        tipo_emojis = {
+            "Fuego": "🔥",
+            "Agua": "💧",
+            "Hierba": "🌱",
+            "Eléctrico": "⚡",
+            "Roca": "🗻",
+            "Hielo": "❄️",
+            "Psíquico": "🔮",
+            "Lucha": "🥊",
+            "Venenoso": "☠️",
+            "Volador": "🕊️",
+            "Fantasma": "👻",
+            "Fantasma / Psíquico": "👻 / 🔮",
+            "Bicho": "🐛",
+            "Dragón": "🐉",
+            "N/A": "❓"
+        }
+        # Obtener el emoji correspondiente al tipo del Pokémon
+        emoji = tipo_emojis.get(self.tipo, "❓")
+        # Formatear la cadena de salida
+        return f"{self.nombre} ({self.tipo}) {emoji}"
